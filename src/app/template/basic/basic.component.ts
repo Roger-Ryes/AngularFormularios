0eed789e3829248967cce8f0a4acb097d8867c50
Roger-Rys <roger.reyesm97@gmail.com>
2022-04-06 07:26:56 -0500

    Condicional en input number

--- src/app/template/basic/basic.component.ts
+++ src/app/template/basic/basic.component.ts
@@ -12,7 +12,13 @@ export class BasicComponent {
   @ViewChild("myForm") myForm!:NgForm;
 
   productValidate():boolean{
-    return this.myForm?.controls['producto']?.invalid && this.myForm?.controls['producto']?.touched
+    return  this.myForm?.controls['producto']?.invalid 
+            && this.myForm?.controls['producto']?.touched
+  }
+
+  priceValidate(): boolean{
+    return  this.myForm?.controls['precio']?.value<0 
+            && this.myForm?.controls['precio']?.touched
   }
 
   save(){
