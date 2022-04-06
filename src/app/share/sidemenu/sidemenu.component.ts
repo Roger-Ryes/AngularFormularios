05dd5c1b4353c47d6ef0236220de3b3c17ea41bf
Roger-Rys <roger.reyesm97@gmail.com>
2022-04-05 21:35:22 -0500

    ShareMenu, routerLink, routerLinkActive,

--- src/app/share/sidemenu/sidemenu.component.ts
+++ src/app/share/sidemenu/sidemenu.component.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface MenuItem{
+  text: String,
+  route: String,
+  active?: String
+}
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
-  ]
+  styles: [`
+    .list-group-item{
+      cursor: pointer;
+    }
+  `]
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  templateMenu: MenuItem[] = [
+    {
+      text: "Básicos",
+      route: "./template/basicos",
+      active: "active"
+    },
+    {
+      text: "Dínamicos",
+      route: "./template/dinamicos" 
+    },
+    {
+      text: "Switches",
+      route: "./template/switches" 
+    }
+  ]
+  
+  reactiveMenu: MenuItem[] = [
+    {
+      text: "Básicos",
+      route: "./reactive/basicos",
+      active: "active"
+    },
+    {
+      text: "Dínamicos",
+      route: "./reactive/dinamicos" 
+    },
+    {
+      text: "Switches",
+      route: "./reactive/switches" 
+    }
+  ]
+  
 
 }
